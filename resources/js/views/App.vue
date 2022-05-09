ebaf6905cb9a6c45526553b18fed8028f2a4e94d
Simone823 <simone.daglio823@outlook.it>
2022-05-09 21:25:32 +0200

    app vue

---
+++ resources/js/views/App.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <!-- App vue -->
+    <div id="app" class="bg-stone-800 text-white">
+
+        <!-- Header nav -->
+        <header class="py-4 px-4 mb-8">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Posts</li>
+                    <li>Categories</li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- Main -->
+        <main class="container">
+            <PostsIndex/>
+        </main>
+    </div>
+    
+</template>
+
+
+<script>
+// Import posts index
+import PostsIndex from '../views/Posts.index.vue';
+
+export default {
+    components: {
+        PostsIndex,
+    },
+
+    name: 'App',    
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+#app {
+    min-height: 100vh;
+}
+
+</style>
