fbbe3299044a52bd9d6b48da5764d9ca6e336061
Simone823 <simone.daglio823@outlook.it>
2022-05-09 18:23:30 +0200

    componente guest home

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
+    <title>@yield('metaTitle')</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    {{-- Link front css --}}
+    <link rel="stylesheet" href="{{asset('css/front.css')}}">
+    
+</head>
+<body>
+
+    {{-- Root --}}
+    <div id="root">
+
+    </div>
+
+    {{-- Script js front --}}
+    <script src="{{asset('js/front.js')}}"></script>
+
+</body>
+</html>
+
