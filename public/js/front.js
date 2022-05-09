4b045f98e7577bda17724e0f9ba849361f64001a
Simone823 <simone.daglio823@outlook.it>
2022-05-09 18:26:32 +0200

    div app front js

--- public/js/front.js
+++ public/js/front.js
@@ -2407,7 +2407,7 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div")
+  return _c("div", { attrs: { id: "app" } })
 }
 var staticRenderFns = []
 render._withStripped = true
